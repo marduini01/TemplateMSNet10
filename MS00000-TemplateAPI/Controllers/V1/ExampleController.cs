c4a0eb0bbe2f7fbd089d099393624fd520ab2119
yoravas <yoravas@gmail.com>
2026-07-06 12:13:21 +0200

    Aggiunto supporto IMediator in ExampleController
    Introdotte le using per Flowify.Contracts e IMediator. Aggiunto il campo mediator e aggiornato il costruttore per l'iniezione delle dipendenze. Inserito commento per possibile utilizzo di mediator.Send() nel metodo Get().

--- MS00000-TemplateAPI/Controllers/V1/ExampleController.cs
+++ MS00000-TemplateAPI/Controllers/V1/ExampleController.cs
@@ -1,4 +1,5 @@
 ﻿using Asp.Versioning;
+using Flowify.Contracts;
 using INPS.ServiceDefault.Logger;
 using INPS.ServiceDefault.Models.DTOs;
 using Microsoft.AspNetCore.Mvc;
@@ -14,12 +15,14 @@ public class ExampleController : ControllerBase
     private readonly IApplicationLogger logger;
     private readonly IWsDatiPensioniConsumer wsDatiPensioniConsumer;
     private readonly ILogger<ExampleController> logger2;
+    private readonly IMediator mediator;
 
-    public ExampleController(IApplicationLogger logger, IWsDatiPensioniConsumer wsDatiPensioniConsumer, ILogger<ExampleController> logger2)
+    public ExampleController(IApplicationLogger logger, IWsDatiPensioniConsumer wsDatiPensioniConsumer, ILogger<ExampleController> logger2, IMediator mediator)
     {
         this.logger = logger;
         this.wsDatiPensioniConsumer = wsDatiPensioniConsumer;
         this.logger2 = logger2;
+        this.mediator = mediator;
     }
     [HttpGet]
     public async Task<IActionResult> Get()
@@ -52,6 +55,7 @@ public class ExampleController : ControllerBase
             Console.WriteLine($"[ERRORE] Chiamata SOAP fallita: {ex.Message}");
         }
 
+        //mediator.Send()
 
         return Ok("Hello from IndexController!");
     }
